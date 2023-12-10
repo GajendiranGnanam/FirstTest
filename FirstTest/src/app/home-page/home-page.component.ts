9f115bb3dee25a9c7a7febd6315049a1731005b6
GajendiranGnanam <gaja.ct@gmail.com>
2023-12-10 10:21:53 +0530

    We cahnge the display message for Home page of application.

--- FirstTest/src/app/home-page/home-page.component.ts
+++ FirstTest/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  displayName: string;
+  constructor() {
+    this.displayName = "diplay by text";
+  }
+  ngOnInit(): void {
+
+  }
+
 
 }
